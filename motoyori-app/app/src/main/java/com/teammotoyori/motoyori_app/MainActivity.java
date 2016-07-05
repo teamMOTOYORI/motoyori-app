eae95646d2756570fecd16ff1774a6d59353b4e9
Dmasa <ikeda@s15.yeskist.net>
2016-07-05 12:52:26 +0900

    MainActivityの修正

--- motoyori-app/app/src/main/java/com/teammotoyori/motoyori_app/MainActivity.java
+++ motoyori-app/app/src/main/java/com/teammotoyori/motoyori_app/MainActivity.java
@@ -2,12 +2,35 @@ package com.teammotoyori.motoyori_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    HashMap<String, String> dic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView TextView1 = (TextView) this.findViewById(R.id.TextView1);
+        TextView TextView2 = (TextView) this.findViewById(R.id.TextView2);
+        EditText editText = (EditText) this.findViewById(R.id.editText);
+        Button button1 = (Button) this.findViewById(R.id.button1);
+
+        HashMap<String,String> map = new HashMap<String,String>();
+
+        try {
+            dic = MotoyoriLib.openFile(getAssets().open("aa"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     }
 }
