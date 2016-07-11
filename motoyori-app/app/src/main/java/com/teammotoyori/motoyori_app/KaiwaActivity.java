da7b8b6a0d17a3cf3d1b6c70068a5b2d2fe01b16
nishi0123 <nishi@s15.yeskist.net>
2016-07-11 10:13:41 +0900

    とりあえず２

---
+++ motoyori-app/app/src/main/java/com/teammotoyori/motoyori_app/KaiwaActivity.java
@@ -0,0 +1,47 @@
+package com.teammotoyori.motoyori_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+public class KaiwaActivity extends AppCompatActivity {
+    TextView textView1;
+    TextView textView2;
+    EditText editText;
+    MotoyoriKun motoyori;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView1 = (TextView) this.findViewById(R.id.textView1);
+        textView2 = (TextView) this.findViewById(R.id.textView2);
+        editText = (EditText) this.findViewById(R.id.editText);
+        Button button1 = (Button) this.findViewById(R.id.button1);
+
+        InputStream is = null;
+        try {
+            is = getAssets().open("test.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        motoyori = new MotoyoriKun(textView2, is);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                textView1.setText(s);
+                motoyori.talk(s);
+            }
+        });
+    }
+}
